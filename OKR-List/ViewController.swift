3daec29ae87648a6b628279075b4602405619344
Alexander Yalchik <ayalchik@distillery.com>
2018-10-31 12:18:25 +0300

    Initial Commit

---
+++ OKR-List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OKR-List
+//
+//  Created by Alexander Yalchik on 10/31/18.
+//  Copyright © 2018 Alexander Yalchik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
